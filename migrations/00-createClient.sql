f4349d3f949d7636c761d2e8990bcb22cac3c3f8
Wilgnne Khawan <wilgnne.kba@gmail.com>
2022-12-18 12:25:22 +0000

    feat: JSON Client Controller

--- migrations/00-createClient.sql
+++ migrations/00-createClient.sql
@@ -1,19 +1,6 @@
-CREATE TABLE address(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    street VARCHAR(20) NOT NULL,
-    address_number INT NOT NULL,
-    district VARCHAR(25) NOT NULL,
-    city VARCHAR(50) NOT NULL,
-    state VARCHAR(30) NOT NULL,
-    country VARCHAR(30) NOT NULL,
-    complement VARCHAR(50)
-);
-
 CREATE TABLE client(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    full_name VARCHAR(100) NOT NULL,
+    fullName VARCHAR(100) NOT NULL,
     cpf CHAR(11) NOT NULL UNIQUE,
-    id_address INT,
-    phone VARCHAR(24) NOT NULL,
-    FOREIGN KEY (id_address) REFERENCES address(id)
+    phone VARCHAR(24) NOT NULL
 );
--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import { MySQLDataBase } from './infra/MySQLDataBase'
 import { EjsMovieController } from './controllers/EjsMovieController'
 import { JsonMovieController } from './controllers/JsonMovieController'
 import { SqlMovieDao } from './dao/SqlMovieDao'
+import { SqlClientDao } from './dao/SqlClientDao'
+import { JsonClientController } from './controllers/JsonClientController'
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -16,13 +18,21 @@ app.use(express.json())
 
 const database = MySQLDataBase.getConnection()
 const sqlMovieDao = new SqlMovieDao(database)
+const sqlClientDao = new SqlClientDao(database)
+
 const jsonMovieController = new JsonMovieController(sqlMovieDao)
 const ejsMovieController = new EjsMovieController(sqlMovieDao)
+
+const jsonClientController = new JsonClientController(sqlClientDao)
 
 const jsonMovieRouter = routerControllerAdpter('/api/movie', jsonMovieController)
 const ejsMovieRouter = routerControllerAdpter('/movie', ejsMovieController)
 
+const jsonClientRouter = routerControllerAdpter('/api/client', jsonClientController)
+
 app.use(jsonMovieRouter)
 app.use(ejsMovieRouter)
+
+app.use(jsonClientRouter)
 
 export default app
