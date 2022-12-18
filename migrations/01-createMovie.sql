b21ed0ca8d3f745747ac5b913b6aa28460479d89
Wilgnne Khawan <wilgnne.kba@gmail.com>
2022-12-18 11:50:50 +0000

    chore: use camelCase in database

--- migrations/01-createMovie.sql
+++ migrations/01-createMovie.sql
@@ -1,16 +1,12 @@
-CREATE TABLE genre (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255)
-);
-
 CREATE TABLE movie (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    release_year YEAR NOT NULL,
-    lease_time TIMESTAMP NOT NULL,
-    lease_value DECIMAL(10, 2) NOT NULL,
-    fee_value DECIMAL(10, 2) NOT NULL,
-    id_genre INT NOT NULL,
+    title VARCHAR(521) NOT NULL,
+    releaseDate DATE NOT NULL,
+    leaseTime DECIMAL(10, 2) NOT NULL,
+    leaseValue DECIMAL(10, 2) NOT NULL,
+    feeValue DECIMAL(10, 2) NOT NULL,
     synopsis VARCHAR(1024),
     amount INT,
-    FOREIGN KEY (id_genre) REFERENCES genre(id)
+    posterUrl VARCHAR(2048),
+    coverUrl VARCHAR(2048)
 );
